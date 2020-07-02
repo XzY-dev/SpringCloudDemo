7b455484d32f92471327fd3f1fc0a7357b80cd91
xingzhnegyu <383619093@qq.com>
2020-07-02 22:01:05 +0800

    Gateway-04 常用的predicate配置

---
+++ cloud-gateway-gateway/src/test/java/Test.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author XZY
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/7/2 21:47
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
