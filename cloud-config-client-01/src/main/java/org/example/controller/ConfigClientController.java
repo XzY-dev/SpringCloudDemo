72d6ae6bccb88fc8cfe376ed70de4214500da6f5
xingzhnegyu <383619093@qq.com>
2020-07-04 16:53:09 +0800

    Config Client02 手动动态刷新

--- cloud-config-client-01/src/main/java/org/example/controller/ConfigClientController.java
+++ cloud-config-client-01/src/main/java/org/example/controller/ConfigClientController.java
@@ -1,16 +1,18 @@
 package org.example.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author XZY
- * @version V1.0
+ * @version V1.0 -curl -X POST http://localhost:3355/actuator/refresh
  * @Package org.example.controller
  * @date 2020/7/4 16:30
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
