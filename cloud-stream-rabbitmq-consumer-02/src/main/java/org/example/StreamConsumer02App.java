5cdbb4429905ff47011f076ee3ff2feb25272522
xingzhnegyu <383619093@qq.com>
2020-07-09 22:46:31 +0800

    stream consumer

---
+++ cloud-stream-rabbitmq-consumer-02/src/main/java/org/example/StreamConsumer02App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author XZY
+ * @version V1.0
+ * @Package org.example
+ * @date 2020/7/9 22:19
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamConsumer02App {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumer02App.class, args);
+    }
+}
