9f582f8fd6c79fc0fbceb9f1986ba25a02bd2483
xingzhnegyu <383619093@qq.com>
2020-07-12 17:24:14 +0800

    NacosConfig

---
+++ cloud-alibaba-config-client/src/main/java/org/example/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package org.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author XZY
+ * @version V1.0
+ * @Package org.example.controller
+ * @date 2020/7/12 16:58
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
