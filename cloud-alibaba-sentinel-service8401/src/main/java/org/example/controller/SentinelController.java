acccd281a859eca5b878f2f2da08680b93b3b2c7
xingzhnegyu <383619093@qq.com>
2020-07-16 22:53:54 +0800

    test hotkey

--- cloud-alibaba-sentinel-service8401/src/main/java/org/example/controller/SentinelController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/org/example/controller/SentinelController.java
@@ -1,6 +1,9 @@
 package org.example.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -40,5 +43,15 @@ public class SentinelController {
     public String testE(){
         int a = 10/0;
         return "--------testE";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p1",required = false) String p2){
+        return "testHotKey";
+    }
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "---------------deal_testHotKey,o(╥﹏╥)o";
     }
 }
